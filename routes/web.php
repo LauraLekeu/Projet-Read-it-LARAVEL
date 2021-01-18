3daed13368752d6f37bb0bcd57ab3e4225b496f6
laura <lauralekeu13@gmail.com>
2021-01-18 13:44:13 +0100

    Route par défaut - Liste des posts

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('templates.index');
-});
+/*
+|--------------------------------------------------------------------------
+| Routes des posts
+|--------------------------------------------------------------------------
+*/
+use App\Http\Controllers\Posts;
+
+// ROUTE PAR DEFAUT
+// PATTERN: /
+// CTRL: Posts
+// ACTION: index
+  Route::get('/', [Posts::class, 'index'])->name('blog');
+
+// LISTE DES POSTS
+// PATTERN: /posts
+// CTRL: Posts
+// ACTION: index
+  Route::get('/posts', [Posts::class, 'index'])->name('posts.index');
